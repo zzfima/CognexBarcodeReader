e2e8aa882fe96acdcd6c51dacebfe1c757432389
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-05-02 15:58:23 +0300

    move to Microosoft.SDK

--- BarcodeReader/BarcodeReader.UnitTest/Properties/AssemblyInfo.cs
+++ BarcodeReader/BarcodeReader.UnitTest/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("BarcodeReader.UnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BarcodeReader.UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("141b490b-a00b-45fa-bc55-0dd3647e4778")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
